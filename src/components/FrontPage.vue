9f8d7a6696fe147d8cc9ef3d931dc1ba77dbe906
Mochammad Nur Afandi <nafandi@outlook.jp>
2026-08-30 13:17:39 +0700

    Consume fetched data to html frontpage

--- src/components/FrontPage.vue
+++ src/components/FrontPage.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
-
-//import { useFetch } from '@vueuse/core';
-// todo : write implemtation of fetch
+import { ref } from 'vue';
+let temp = ref("(loading)");
+let humidity = ref("(loading)");
 const url = "/api/";
 async function fetchurl() {
-  fetch(url).then(function (response) { return response.json() }).then(function (json) {
-    console.log("humidity : " + json.humidity);
-    console.log("temperature : " + json.temperature)
-  })
+  let sensor = await fetch(url);
+  let data = await sensor.json();
+  temp.value = data.temperature;
+  humidity.value = data.humidity;
 }
+// call api on browser load
+window.addEventListener("DOMContentLoaded", fetchurl);
+// make it automatic
+setInterval(fetchurl, 30000);
 </script>
 <template>
-<h1>Temperature Monitor - esp32</h1>
+<h2>Temperature Monitor - esp32</h2>
 <h2>cheap temperature monitor for your home</h2>
-<h3>Temperature : </h3>
-<h3>Humidity: </h3>
-<h2>Refresh</h2>
-<fetchurl />
+<h3>Temperature : {{temp}} C</h3>
+<h3>Humidity: {{humidity}} %</h3>
 </template>
